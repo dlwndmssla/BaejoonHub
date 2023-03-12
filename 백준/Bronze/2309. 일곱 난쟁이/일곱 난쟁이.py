9ac8a1cb55b0132b21fb0598cd9b88f74daa8855
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2023-03-12 16:05:42 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,22 @@
+import sys
+#input = sys.stdin.readline
+a = []
+for k in range(9):
+    a.append(int(input()))
+a.sort()
+
+b = sum(a)
+for i in range(8):
+    for j in range(i+1,9):
+        c = b - a[i] - a[j]    
+        if c == 100:
+            break
+    if c == 100:
+            break
+
+ex1 = a
+ex1.remove(a[j])
+ex1.remove(a[i])
+
+for k in range(len(ex1)):
+    print(ex1[k])
