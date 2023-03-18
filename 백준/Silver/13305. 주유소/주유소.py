80c726cd385a8357b699b6defd4ae7c28a98afb9
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2023-03-18 18:51:12 +0900

    [Silver III] Title: 주유소, Time: 128 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+a = int(input())
+ex1 = list(map(int, input().split()))
+ex3 = list(map(int, input().split()))
+for i in range(a-1):
+    for j in range(i,a-1):
+        if ex3[j] < ex3[i]:
+            break
+        else:
+            ex3[j] = ex3[i]
+cost = 0
+for i in range(a-1):
+    cost += ex3[i]*ex1[i]
+    
+print(cost)
