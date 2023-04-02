a0ff9dbf04ccbf1f87477aeb7dead05d036c225f
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2023-04-02 23:46:02 +0900

    [Bronze III] Title: 등장하지 않는 문자의 합, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/3059. 등장하지 않는 문자의 합/등장하지 않는 문자의 합.py
@@ -0,0 +1,10 @@
+for j in range(int(input())):
+    alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    num = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
+    ex = str(input())
+    ex = set(ex)
+    for i in ex:
+        k = alpha.index(i)
+        num[k]=0
+        
+    print(sum(num))
