a655a27839d4c0ee7807593c508530435ad4e5e5
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2023-04-10 13:26:49 +0900

    [Silver IV] Title: 듣보잡, Time: 100 ms, Memory: 44060 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+a,b = map(int, input().split())
+ex1 = set()
+ex2 = set()
+for i in range(a):
+    ex1.add(sys.stdin.readline().rstrip())
+for i in range(b):
+    ex2.add(sys.stdin.readline().rstrip())
+
+x = []
+
+if a >= b:
+    for i in ex2:
+        if i in ex1:
+            x.append(i)      
+else:
+    for i in ex1:
+        if i in ex2:
+            x.append(i)
+            
+x.sort()
+print(len(x))
+for i in x:
+    print(i)
