c0c1f709600619ece3cf8084f8c1c26062885112
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2024-06-27 21:58:06 +0900

    [Silver I] Title: 그림, Time: 472 ms, Memory: 36980 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,44 +1,40 @@
+#그림
 import sys
-sys.setrecursionlimit(10 ** 8)
+from collections import deque
+sys.setrecursionlimit(10 ** 7)
 input = sys.stdin.readline
 
-a,b = map(int, input().split())
-graph = []
-for i in range(a):
-    ex = list(map(int, input().split()))
-    graph.append(ex)
-    
+n,m = list(map(int,input().split()))
 
-def dfs(graph,x,y,visited):
-    node = 1000*x+y
-    visited.add(node)
-    for t in range(4):
-        x0 = [-1,1,0,0][t] + x
-        y0 = [0,0,-1,1][t] + y
-        if 0 <= x0 <= b-1 and 0 <= y0 <= a-1:
-            node0 = 1000*x0+y0
-            if graph[y0][x0] == 1 and node0 not in visited :
-                visited.add(node0)
-                dfs(graph, x0, y0,visited)
-            elif node0 in visited:
-                continue
+graph = [list(map(int,input().split())) for i in range(n)]
 
-c0 = 0
-k = 0
-num = 0
 
-visited = set()
-
-for y in range(a):
-    for x in range(b):
-        node = 1000*x+y
-        if node not in visited and graph[y][x] == 1:
-            dfs(graph,x,y,visited)
-            c1 = len(visited)
-            k = max(k, c1-c0)
-            c0 = c1
-            num += 1
+def bfs(v):
+    global ans
+    y,x = v
+    visited[y][x] = True
+    queue = deque([v])
+    a1 = 1
+    while queue:
+        y,x = queue.popleft()
+        for t in range(4):
+            y0,x0 = y+[0,0,-1,1][t],x+[-1,1,0,0][t]
+            if not ((0<= y0 <=n-1) and (0<= x0<=m-1)) : continue
+            if visited[y0][x0]: continue
+            if graph[y0][x0] == 0: continue
+            visited[y0][x0] = True
+            queue.append([y0,x0])
+            a1 += 1
             
-print(num)
-print(k)  
-         
+    ans.append(a1)
+     
+visited = [[False for i in range(m)] for j in range(n)]   
+ans = [0]
+for y1 in range(n):
+    for x1 in range(m):
+        if (graph[y1][x1] == 1) and not visited[y1][x1]:
+            v = [y1,x1]
+            bfs(v)
+
+print(len(ans)-1)
+print(max(ans))
