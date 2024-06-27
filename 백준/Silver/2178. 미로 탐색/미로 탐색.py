fdf36818b3896af0ab561ed733f886b749260615
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2024-06-27 22:10:54 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,35 +1,31 @@
+#미로탐색
 import sys
 from collections import deque
+sys.setrecursionlimit(10 ** 7)
 input = sys.stdin.readline
 
-n,m = map(int, input().split())
-graph = []
+n,m = list(map(int,input().split()))
 
-for i in range(n):
-    ex = list(map(str, input().rstrip()))
-    graph.append(ex)
-     
-def bfs(graph,x,y,visited):
-    visited[y][x] = True
-    queue = deque()
-    node = [y,x]
-    queue.append(node)
-    count0[y][x] = 1
+graph = [list(map(str,input())) for i in range(n)]
+
+def bfs(v):
+    y,x = v
+    visited[y][x] = 1
+    queue = deque([v])
     while queue:
-        v = queue.popleft()
-        y, x = v[0], v[1]
+        y,x = queue.popleft()
+        cnt = visited[y][x]+1
         for t in range(4):
-            x0 = x + [-1,1,0,0][t]
-            y0 = y + [0,0,-1,1][t]
-            if 0 <= x0 <= m-1 and 0 <= y0 <= n-1:
-                if graph[y0][x0] == '1' and not visited[y0][x0]:
-                    visited[y0][x0] = True
-                    queue.append([y0,x0])
-                    if count0[y0][x0] ==0:
-                         count0[y0][x0] = count0[y][x]+1
-                    
-visited = [[False for i in range(m)] for j in range(n)]
-count0 = [[0 for i in range(m)] for j in range(n)]
-x,y = 0,0
-bfs(graph,x,y,visited)
-print(count0[-1][-1])              
+            y0,x0 = y+[0,0,-1,1][t],x+[-1,1,0,0][t]
+            if not ((0<= y0 <=n-1) and (0<= x0<=m-1)) : continue
+            if visited[y0][x0] != -1: continue
+            if graph[y0][x0] == '0': continue
+            visited[y0][x0] = cnt
+            queue.append([y0,x0])
+
+     
+visited = [[-1 for i in range(m)] for j in range(n)]   
+
+bfs([0,0])
+
+print(visited[-1][-1])
