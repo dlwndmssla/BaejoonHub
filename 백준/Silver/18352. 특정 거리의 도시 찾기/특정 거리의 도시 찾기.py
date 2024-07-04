41c085619815008a74602fa5fb5b67503ca94aa1
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2024-07-04 13:03:14 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 3076 ms, Memory: 172716 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,37 +1,35 @@
 import sys, heapq
-input = sys.stdin.readline
+input = sys.stdin.readline 
 
-n,m,k,x = map(int,input().split())
-INF = int(10e8)
-graph = [[] for i in range(n+1)]
-
+n,m,k,x = list(map(int,input().split()))
+INF = 1e8
+graph = [ [] for i in range(n+1) ]
+distance = [ INF for i in range(n+1) ]
 for i in range(m):
-    u,v= map(int,input().split())
-    if [v,1] not in graph[u]:
-        graph[u].append([v,1])
+    a,b = list(map(int,input().split()))
+    graph[a].append((b,1))
 
-def dijkstra(start):
-    distances = [INF] * (n+1)
-    distances[start] = 0
+import heapq
+
+def dijkstra(graph,x):
+    distance[x] = 0
     queue = []
-    heapq.heappush(queue, (distances[start], start))
+    heapq.heappush(queue, (0, x)) # 우선순위, 값 형태로 들어간다.
     while queue:
-        dist, now = heapq.heappop(queue)
-        if distances[now] < dist:
-            continue
-        for now0, weight in graph[now]:
-            dist0 = dist + weight
-            if dist0 < distances[now0]:
-                distances[now0] = dist0
-                heapq.heappush(queue, (dist0, now0))
+        dist,now = heapq.heappop(queue)
+        for now0,cost in graph[now]:
+            dist0 = distance[now]+cost
+            if dist0 >= distance[now0]: continue
+            distance[now0] = dist0
+            heapq.heappush(queue, (dist0, now0))
 
-    return distances
+    return distance
 
-distances = dijkstra(x)
+dist1 = dijkstra(graph,x)
 
-if distances[1:].count(k) > 0:
-    for i in range(1,len(distances)):
-        if distances[i] == k:
+if dist1[1:].count(k) > 0:
+    for i in range(1,len(dist1)):
+        if dist1[i] == k:
             print(i)
 else:
     print(-1)
