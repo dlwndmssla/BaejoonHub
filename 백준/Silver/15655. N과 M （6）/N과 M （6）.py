f1a03a170440ab080397fbe5567c9cb431519e84
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2024-07-05 23:31:56 +0900

    [Silver III] Title: N과 M (6), Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,22 @@
+n,m = list(map(int,input().split()))
+
+numbers = list(map(int,input().split()))
+numbers.sort()
+visited = [False for i in range(n)]
+s = []
+def bk(k):
+    if m == len(s):
+        print(" ".join(map(str,s)))
+        return
+    
+    for i in range(len(numbers)):
+        if visited[i]: continue
+        if len(s) != 0:
+            if s[-1] > numbers[i]: continue
+        visited[i] = True
+        s.append(numbers[i])
+        bk(k+1)
+        s.pop()
+        visited[i] = False
+        
+bk(0)
