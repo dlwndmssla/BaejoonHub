aed3a3ab1bfb76e08d20cf29aa04e4a7969bd3bc
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2024-07-17 12:28:19 +0900

    [Silver I] Title: 회의실 배정, Time: 292 ms, Memory: 59068 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,17 +1,11 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-ex = []
-max0 = 0
-for i in range(n):
-    ex1 = list(map(int, input().split()))
-    max0 = max(max0,ex1[1])
-    ex.append(ex1)
-
-ex = sorted(ex, key=lambda x: (x[1], x[0]))
-meetings = [ex[0]]
+ 
+time= sorted([list(map(int,input().split())) for i in range(n)], key=lambda x: (x[1], x[0]))
+meetings = [time[0]]
 for i in range(1,n):
-    if meetings[-1][1]<= ex[i][0]:
-        meetings.append(ex[i])
+    if meetings[-1][1]<= time[i][0]:
+        meetings.append(time[i])
 
 print(len(meetings))
