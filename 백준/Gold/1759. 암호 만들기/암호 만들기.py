b2e23fe78f569b1813a4e957a9f9d234817a398e
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2024-07-23 23:56:58 +0900

    [Gold V] Title: 암호 만들기, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+def bk():
+    if len(s) == l:
+        vowels = sum([ s0 in s for s0 in ['a','e','i','o','u']])
+        consonants = l-vowels
+        if vowels >= 1 and consonants >= 2:
+            print(''.join(map(str,s)))
+        return
+    
+    for i in range(c):
+        if visited[i]: continue
+        if len(s) != 0:
+            if s_idx[-1] > i: continue
+        visited[i] = True
+        s.append(str_list[i])
+        s_idx.append(i)
+        bk()
+        s.pop()
+        s_idx.pop()
+        visited[i] = False
+        
+l,c = list(map(int,input().split()))
+str_list = sorted(list(map(str,input().split())))
+
+
+s = []
+s_idx = []
+visited = [False for i in range(c)]
+
+bk()
