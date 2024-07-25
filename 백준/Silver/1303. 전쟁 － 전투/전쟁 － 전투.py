d15c8a555ee08cc97abbf342730f632613c2ca0c
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2024-07-25 14:13:11 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 136 ms, Memory: 31132 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -1,39 +1,34 @@
 import sys
 sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
-n,m = map(int, input().split())
-battle = []
-for i in range(m):
-    ex = list(map(str,input().rstrip()))
-    battle.append(ex)
 
-visited = [[False for i in range(n)] for j in range(m)]
-x,y = 0,0
-count0 = 0
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
 def dfs(graph, x, y, visited):
     visited[y][x] = True
     for k in range(4):
-        i = [0,0,-1,1][k]
-        j = [-1,1,0,0][k]
-        if 0 <=x+i<= n-1 and 0 <=y+j<= m-1 and not visited[y+j][x+i] and battle[y][x] == battle[y+j][x+i]:
-            visited[y+j][x+i] = True
-            dfs(graph,x+i,y+j,visited)
-                    
-wc,bc = 0,0
-count0 = 0
-graph = battle
-for s in range(n):
-    for t in range(m):
-        x,y = s,t
-        if visited[y][x] == True:
-            continue
-        dfs(graph, x, y, visited)
+        y0,x0= y+dy[k],x+dx[k]
+        if not (0 <=x0<= n-1 and 0 <=y0<= m-1): continue
+        if visited[y0][x0]: continue
+        if battle[y][x] != battle[y0][x0]: continue
+        visited[y0][x0] = True
+        dfs(graph,x0,y0,visited)
+        
+        
+n,m = map(int, input().split())
+battle = [list(map(str,input().rstrip())) for i in range(m)]
+visited = [[False for i in range(n)] for j in range(m)]
+wc,bc,cnt = 0,0,0
+
+for x in range(n):
+    for y in range(m):
+        if visited[y][x] == True: continue
+        dfs(battle, x, y, visited)
         count_ex = sum([sum(visited[kk]) for kk in range(len(visited))])
-        
         if battle[y][x] == 'W':
-            wc += pow(count_ex- count0,2)
+            wc += pow(count_ex- cnt,2)
         else:
-            bc += pow(count_ex- count0,2)
-        count0 = count_ex    
-        
+            bc += pow(count_ex- cnt,2)
+        cnt = count_ex    
+
 print(wc, bc)
