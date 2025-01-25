e75888360cf40c0928007eb94af48b4a8b8ed73b
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2025-01-25 15:47:56 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,10 +1,4 @@
-max = 0
-
-for i in range(9):
-    a = int(input())
-    if a > max:
-        max = a
-        k = i+1
-    if i == 8:
-        print(max)
-        print(k)
+nums = [int(input()) for i in range(9)]
+max_num = max(nums)
+print(max_num)
+print(nums.index(max_num)+1)
