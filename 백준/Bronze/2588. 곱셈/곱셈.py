d727aca76d27d1233d5248a876689ae6a0d04ff0
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2025-01-25 15:38:23 +0900

    [Bronze III] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,9 +1,6 @@
-a = int(input())
-b = str(input())
-d = 0
-for i in range(3):
-    c = a*int(b[2-i])
-    print(c)
-    d += c*pow(10,i)
-    
-print(d)
+num1 = int(input())
+num2 = str(input())
+num2_list = [int(i) for i in num2]
+a = [i*num1 for u,i in enumerate(num2_list[::-1])]
+for i in a: print(i)
+print(num1*int(num2))
