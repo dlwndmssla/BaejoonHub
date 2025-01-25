fe3bbf1b98d309db40420fc944b8eb5bf166dc54
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2025-01-25 14:35:02 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,11 +1,4 @@
-a,b,c = map(int, input().split())
-
-if a==b==c:
-    print(10000 + a*1000)
-elif a ==b or a ==c or b== c:
-    if a==b or a==c:
-        print(1000 + a*100)
-    elif b == c:
-        print(1000 + b*100)
-else:
-    print(100*max(a,b,c))
+dice = list(map(int,input().split()))
+sort_dice = sorted(set(dice))[::-1]
+print([10000+sort_dice[0]*1000,1000+sum([i*(dice.count(i)==2) for i in sort_dice])*100
+,max(sort_dice)*100][len(sort_dice)-1])
