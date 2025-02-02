e1d186782dbf48e68042c315c526e31ed90f2f82
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2025-02-02 17:32:33 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,15 +1,9 @@
-import sys
-input = sys.stdin.readline
-N = int(input())
-
-for i in range(N):
-    a = int(input())
-    b = int(input())
-    ex1 = [i for i in range(1,b+1)]
-    ex2 = []
-    for j in range(a):
-        for k in range(b):
-            ex2.append(sum(ex1[:k+1])) 
-        ex1 = ex2
-        ex2 = []   
-    print(ex1[-1]) 
+import itertools
+for _ in range(int(input())):
+    k,n = int(input()),int(input())
+    downstair = [i for i in range(n+1)]
+    for s in range(k):
+        now = list(itertools.accumulate(downstair))
+        # print(now)
+        downstair = now
+    print(now[-1])
