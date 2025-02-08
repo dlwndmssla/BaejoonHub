eb2eb265f6d4a39f3f676cd80d505f4d84bada79
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2025-02-08 11:40:28 +0900

    [Bronze I] Title: 오르막길, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2846. 오르막길/오르막길.py
@@ -0,0 +1,13 @@
+n = int(input())
+nums = list(map(int,input().split()))
+nums_sign = [0 for i in range(len(nums))]
+ans = 0
+for i,x in enumerate(nums[1:]):
+    if (x > nums[i]) and (nums_sign[i] >= 0):
+        nums_sign[i+1] += nums[i+1]-nums[i]+nums_sign[i]
+        if ans < nums_sign[i+1]:
+            ans = nums_sign[i+1]
+        
+# print(nums)
+# print(nums_sign)
+print(ans)
