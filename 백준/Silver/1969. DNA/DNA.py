65b9c2666c0f9c009b429bfd5aff74d77d01cfe7
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2025-02-08 13:53:11 +0900

    [Silver IV] Title: DNA, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1969. DNA/DNA.py
@@ -0,0 +1,14 @@
+
+n,m = list(map(int,input().split()))
+dna_list = sorted([input() for i in range(n)])
+
+
+ans = ''
+cnt = 0
+for i in zip(*dna_list):
+    cnt_list = list((list(i).count(j) for j in 'ACGT'))
+    max_idx = cnt_list.index(max(cnt_list))
+    ans += 'ACGT'[max_idx]
+    cnt += n-max(cnt_list)
+print(ans)  
+print(cnt)  
