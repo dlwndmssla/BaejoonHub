4486f51ef71769a99ce13b98e1b278e93faffe99
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2025-02-08 13:14:14 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
@@ -1,12 +1,6 @@
-
 import itertools
 import sys
 input = sys.stdin.readline
-
-n = int(input())
-
-room = [str(input()) for i in range(n)]
-room_space = [[int(r[i]=='.') for i in range(n)] for r in room]
 
 def count_space(list0):
     for y in range(n):
@@ -15,6 +9,11 @@ def count_space(list0):
                 list0[y][x] += list0[y][x-1]
                 
     return sum(list0,[]).count(2)
+
+
+n = int(input())
+room = [str(input()) for i in range(n)]
+room_space = [[int(r[i]=='.') for i in range(n)] for r in room]
 
 room_space_r = room_space
 room_space_c = list(list(i) for i in zip(*room_space))
