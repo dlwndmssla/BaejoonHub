bb1de4b776b60e9d54a8bdc49dc0df28211771da
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2025-02-09 13:09:08 +0900

    [Silver V] Title: 행렬 곱셈, Time: 92 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
@@ -3,12 +3,7 @@ input = sys.stdin.readline
 n1,m1 = list(map(int,input().split()))
 list1 = [list(map(int,input().split())) for i in range(n1)]
 n2,m2 = list(map(int,input().split()))
-list2 = [list(map(int,input().split())) for i in range(n2)]
-list3 = [list(i) for i in zip(*list2)]
-# print(list1)
-# print(list2)
-# print(list3)
+list2 = [list(i) for i in zip(*[list(map(int,input().split())) for i in range(n2)])]
 
-ans = [[ sum([x[0]*x[1] for x in zip(i,j)]) for i in list3] for j in list1]
-for i in ans:
-    print(*i)
+ans = [[ sum([x[0]*x[1] for x in zip(i,j)]) for i in list2] for j in list1]
+for i in ans:    print(*i)
