14b16a62313e644149a98182053a6a888c132ac6
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2025-02-10 21:29:27 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 33948 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,10 +1,7 @@
-n = int(input())
-paper = list()
-
-for i in range(n):
-    a,b = map(int,input().split())
-    for x in range(a,a+10):
-        for y in range(b,b+10):
-            paper.append((x,y))
-            
-print(len(set(paper)))
+import sys
+input = sys.stdin.readline
+space = []
+for i in range(int(input())):
+    x,y = list(map(int,input().split()))
+    space.extend(sum([[ (i,j) for i in range(x,x+10)] for j in range(y,y+10)],[]))
+print(len(set(space)))
