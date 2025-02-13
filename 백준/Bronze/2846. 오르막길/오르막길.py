aa1db18f2d3ba91c8659ff5cb61bd8e67724fe7b
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2025-02-13 11:17:16 +0900

    [Bronze I] Title: 오르막길, Time: 32 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/2846. 오르막길/오르막길.py
+++ 백준/Bronze/2846. 오르막길/오르막길.py
@@ -1,10 +1,10 @@
 n = int(input())
 nums = list(map(int,input().split()))
-nums_sign = [0 for i in range(len(nums))]
+nums_sign = [0 for i in range(len(nums))] #모든수를 0으로 시작. 증가하는지 여부(>임 >= 아님)
 ans = 0
-for i,x in enumerate(nums[1:]):
-    if (x > nums[i]) and (nums_sign[i] >= 0):
-        nums_sign[i+1] += nums[i+1]-nums[i]+nums_sign[i]
+for i,x in enumerate(nums[1:]): 
+    if (x > nums[i]): #이전수가 현재수보다 작고, 이전수가 감소하지 않았다면
+        nums_sign[i+1] += nums[i+1]-nums[i]+nums_sign[i] # 이전수와 현재수의 차에 이전까지의 누적값을 더함
         ans = max(ans,nums_sign[i+1])
 
 print(ans)
