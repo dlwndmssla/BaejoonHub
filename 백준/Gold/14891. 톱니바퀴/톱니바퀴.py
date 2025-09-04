828924684c8420430107391a08faf119aa7b74d0
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2025-09-04 13:47:25 +0900

    [Gold V] Title: 톱니바퀴, Time: 60 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -1,34 +1,14 @@
 from collections import deque
 chain = {k:deque([int(i) for i in str(input())]) for k in range(4)}
-check = {0:[(2,1),(4,3),(6,5)],
-1:[(1,2),(4,3),(6,5)],
-2:[(6,5),(3,4),(1,2)],
-3:[(5,6),(3,4),(1,2)]}
-
+check = {0:[(2,1),(4,3),(6,5)],1:[(1,2),(4,3),(6,5)],2:[(6,5),(3,4),(1,2)],3:[(5,6),(3,4),(1,2)]}
 for _ in range(int(input())):
     x,y = list(map(int,input().split()))
-    x = x-1
-    conn = [chain[i][j] for i in [0,1,2,3] for j in [-2,2] ]
-    d = [0, 0, 0, 0]
-    d[x] = y
-    # print(conn)
+    x,d = x-1,[0, 0, 0, 0]
+    d[x],conn = y,[chain[i][j] for i in [0,1,2,3] for j in [-2,2] ]
     for i,j in check[x]:
-        # print(i//2,j//2)
-        if (d[j//2] != 0) and conn[i] != conn[j]:
-            d[i//2] = -d[j//2]
-    # print("d",d)
-
-    for i,b in chain.items():
-        a = d[i]
-        if a == 1:
-            c = b.pop()
-            b.appendleft(c)
-        elif a == -1:
-            c = b.popleft()
-            b.append(c)
+        if (d[j//2] != 0) and conn[i] != conn[j]: d[i//2] = -d[j//2]
+    for a,(i,b) in zip(d,chain.items()):
+        if a == 1:b.appendleft(b.pop())
+        elif a == -1:b.append(b.popleft())
         chain[i] = b
-        # print(chain[i])
-
 print(sum((chain[i][0])*pow(2,i) for i in range(4)))
-
-
