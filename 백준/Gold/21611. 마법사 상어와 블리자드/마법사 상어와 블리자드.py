a465ce5b0c31fc308dceb77360e22b9eb85c4e41
dlwndmssla <122724937+dlwndmssla@users.noreply.github.com>
2025-10-01 15:09:27 +0900

    [Gold I] Title: 마법사 상어와 블리자드, Time: 672 ms, Memory: 35116 KB -BaekjoonHub

---
+++ 백준/Gold/21611. 마법사 상어와 블리자드/마법사 상어와 블리자드.py
@@ -0,0 +1,84 @@
+
+
+global n,grid,boom
+
+from collections import deque
+
+def booming(ball):
+    k,new_ball,g = 0,dict(),deque([])
+    while True:
+        v = ball.get(k,0)
+        k += 1
+        if not v and k > 1: continue
+        if v in g: g.append(v); continue
+        if len(g) < 4:
+            new_ball.update({y:g[0] for y in range(len(new_ball),len(new_ball)+len(g))})
+            g = deque([v])
+        else: boom[g[0]] += len(g) 
+        g = deque([v])
+        if v != -1: continue 
+        new_ball[len(new_ball)] = v
+        return new_ball
+
+    
+def next_ball(ball):
+    k,v,new_ball,g,l = 0,0,{},deque([0]),-1
+    while l < n*n and v != -1:
+        v = ball.get(k,0)
+        k += 1
+        if v in g: g.append(v); continue
+        new_ball[l] = len(g)
+        l += 1
+        new_ball[l] = g[0]
+        l += 1
+        g = deque([v])
+        
+    new_ball.pop(-1)
+    return new_ball
+
+
+def do_boom(m,ball):
+    dirs = {1:(-1,0),2:(1,0),3:(0,-1),4:(0,1)}
+    d,s = m
+    dy,dx = dirs[d]
+    for s0 in range(1,s+1):
+        ball_k = grid[(dy*s0+int(n/2),dx*s0+int(n/2))]
+        if ball_k in ball: ball.pop(ball_k)
+
+    ball[max(ball.keys())+1] = -1
+
+    while True:
+        new_ball = booming(ball)
+        if new_ball == ball: break
+        ball = new_ball
+    new_ball = next_ball(ball)
+    return new_ball
+
+def moveing(n):
+    dirs = [(0,1),(1,0),(0,-1),(-1,0)]
+    cnt,(y,x),d = n*n-1,(0,0),0
+    grid = {(y,x):cnt}
+    cnt -= 1
+    dy,dx = dirs[d]
+    while True:
+        y1,x1 = dy+y,dx+x
+        if not 0<= y1 <=n-1 or not 0<= x1 <=n-1 or (y1,x1) in grid:
+            d = (d+1)%4
+            dy,dx = dirs[d]
+            continue
+        grid[(y1,x1)],y,x = cnt,y1,x1
+        cnt -= 1
+        if cnt < 0: break
+
+    return grid
+
+boom = {1:0, 2:0, 3:0}
+n,m = list(map(int,input().split()))
+grid = moveing(n)
+ball = {grid[(i,j)]:v for i in range(n) for j,v in enumerate(list(map(int,input().split()))) if v}
+for _ in range(m):
+    x = list(map(int,input().split()))
+    if not ball: break
+    ball = do_boom(x,ball)
+
+print(sum([k*v for k,v in boom.items()]))
